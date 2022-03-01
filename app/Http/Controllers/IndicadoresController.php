2e25a152f90e844bded85ff856e83e08f6a64f49
alexneto2022 <alex.neto@hcpgestao.org.br>
2022-03-01 09:10:52 -0300

    atualizador em indicador conttroler

--- app/Http/Controllers/IndicadoresController.php
+++ app/Http/Controllers/IndicadoresController.php
@@ -18,18 +18,18 @@ class IndicadoresController extends Controller
     public function cadastroIndicadores()
     {
         $id_user = Auth::user()->id;
-		$idTela = 4;
-		$validacao = PermissaoUserController::Permissao($id_user, $idTela);
-		if($validacao == "ok") {
-			$indicadores = Indicadores::paginate(20);
+        $idTela = 4;
+        $validacao = PermissaoUserController::Permissao($id_user, $idTela);
+        if ($validacao == "ok") {
+            $indicadores = Indicadores::paginate(20);
             $grupo_indicadores = GrupoIndicadores::all();
             $unidades = Unidades::all();
-            return view('indicadores/indicadores_cadastro', compact('indicadores','grupo_indicadores','unidades'));
-		} else {
-			$validator = "Você não tem Permissão para acessar esta tela!!!";
-			return view('home')
-				->withErrors($validator);
-		}
+            return view('indicadores/indicadores_cadastro', compact('indicadores', 'grupo_indicadores', 'unidades'));
+        } else {
+            $validator = "Você não tem Permissão para acessar esta tela!!!";
+            return view('home')
+                ->withErrors($validator);
+        }
     }
 
     public function telaIndicador($id)
@@ -42,14 +42,18 @@ class IndicadoresController extends Controller
     {
         $id_user = Auth::user()->id;
         $unidades = Unidades::all();
-		$idTela = 4;
-		$validacao = PermissaoUserController::Permissao($id_user, $idTela);
-		if($validacao == "ok") {
-			$input  = $request->all();  
-            if(empty($input['pesq'])) { $input['pesq'] = ""; }
-            if(empty($input['pesq2'])) { $input['pesq2'] = ""; }
+        $idTela = 4;
+        $validacao = PermissaoUserController::Permissao($id_user, $idTela);
+        if ($validacao == "ok") {
+            $input  = $request->all();
+            if (empty($input['pesq'])) {
+                $input['pesq'] = "";
+            }
+            if (empty($input['pesq2'])) {
+                $input['pesq2'] = "";
+            }
             $pesq  = $input['pesq'];
-            $pesq2 = $input['pesq2']; 
+            $pesq2 = $input['pesq2'];
             $grupo_indicadores = GrupoIndicadores::all();
             if ($pesq2 == "1") {
                 $indicadores = Indicadores::where('nome', 'like', '%' . $pesq . '%')->paginate(20);
@@ -58,75 +62,79 @@ class IndicadoresController extends Controller
             } else if ($pesq2 == "3") {
                 $unidade = Unidades::where('nome', 'like', '%' . $pesq . '%')->get();
                 $qtd = sizeof($unidade);
-                if($qtd == 0){
+                if ($qtd == 0) {
                     $indicadores = Indicadores::paginate(20);
                 } else {
-                    $indicadores = Indicadores::where('unidade_id','like','%'.$unidade[0]->id.'%')->paginate(20);
+                    $indicadores = Indicadores::where('unidade_id', 'like', '%' . $unidade[0]->id . '%')->paginate(20);
                 }
+            } else if ($pesq2 == "4") {
+                $indicadores = Indicadores::where('link', 'like', '%' . $pesq . '%')->paginate(20);
             } else {
                 $indicadores = Indicadores::paginate(20);
             }
-            return view('indicadores/indicadores_cadastro', compact('indicadores','pesq','pesq2','grupo_indicadores','unidades'));
-		} else {
-			$validator = "Você não tem Permissão para acessar esta tela!!!";
-			return view('home')
-				->withErrors($validator);
-		}
+            return view('indicadores/indicadores_cadastro', compact('indicadores', 'pesq', 'pesq2', 'grupo_indicadores', 'unidades'));
+        } else {
+            $validator = "Você não tem Permissão para acessar esta tela!!!";
+            return view('home')
+                ->withErrors($validator);
+        }
     }
 
     public function pesquisarIndicadoresGestores(Request $request)
     {
-        $input  = $request->all();  
+        $input  = $request->all();
         $unidade = Auth::user()->unidade_id;
-		if(empty($input['pesq2'])) { $input['pesq2'] = ""; }
-      	$pesq2 = $input['pesq2']; 
+        if (empty($input['pesq2'])) {
+            $input['pesq2'] = "";
+        }
+        $pesq2 = $input['pesq2'];
         $idU = Auth::user()->unidade_id;
-        if($pesq2 != "") {
-             if($idU != 7){
-                $indicadores = Indicadores::where('grupo_id',$pesq2)->where('unidade_id',$unidade)->get();
+        if ($pesq2 != "") {
+            if ($idU != 7) {
+                $indicadores = Indicadores::where('grupo_id', $pesq2)->where('unidade_id', $unidade)->get();
                 $qtd = sizeof($indicadores);
                 $grupo_indicadores = DB::table('grupo_indicadores')
-					->join('indicadores','indicadores.grupo_id','=','grupo_indicadores.id')
-					->select('grupo_indicadores.nome','grupo_indicadores.id')
-                    ->where('indicadores.unidade_id',$idU)
-            		->groupby('grupo_indicadores.nome','grupo_indicadores.id')->get();
+                    ->join('indicadores', 'indicadores.grupo_id', '=', 'grupo_indicadores.id')
+                    ->select('grupo_indicadores.nome', 'grupo_indicadores.id')
+                    ->where('indicadores.unidade_id', $idU)
+                    ->groupby('grupo_indicadores.nome', 'grupo_indicadores.id')->get();
             } else {
-                $indicadores = Indicadores::where('grupo_id',$pesq2)->get();
+                $indicadores = Indicadores::where('grupo_id', $pesq2)->get();
                 $qtd = sizeof($indicadores);
                 $grupo_indicadores = GrupoIndicadores::all();
             }
-            if($qtd == 0) {
+            if ($qtd == 0) {
                 $validator = 'Não existe nenhum Indicador cadastrado nesta Unidade deste Grupo!';
-                return view('indicadores/lista_indicadores', compact('indicadores','pesq2','grupo_indicadores'))
-                  ->withErrors($validator)
-				  ->withInput(session()->flashInput($request->input()));
+                return view('indicadores/lista_indicadores', compact('indicadores', 'pesq2', 'grupo_indicadores'))
+                    ->withErrors($validator)
+                    ->withInput(session()->flashInput($request->input()));
             } else {
-                return view('indicadores/lista_indicadores', compact('indicadores','pesq2','grupo_indicadores'));
+                return view('indicadores/lista_indicadores', compact('indicadores', 'pesq2', 'grupo_indicadores'));
             }
         } else {
-            $indicadores = Indicadores::where('id',0)->get();
+            $indicadores = Indicadores::where('id', 0)->get();
             $validator = 'Selecione um Grupo de Indicadores!';
-            $grupo_indicadores = GrupoIndicadores::orderby('nome','ASC')->get();
-            return view('indicadores/lista_indicadores', compact('indicadores','pesq2','grupo_indicadores'))
+            $grupo_indicadores = GrupoIndicadores::orderby('nome', 'ASC')->get();
+            return view('indicadores/lista_indicadores', compact('indicadores', 'pesq2', 'grupo_indicadores'))
                 ->withErrors($validator)
-				->withInput(session()->flashInput($request->input()));
+                ->withInput(session()->flashInput($request->input()));
         }
     }
 
     public function indicadoresNovo()
     {
         $id_user = Auth::user()->id;
-		$idTela = 4;
-		$validacao = PermissaoUserController::Permissao($id_user, $idTela);
-		if($validacao == "ok") {
-			$unidades = Unidades::all();
+        $idTela = 4;
+        $validacao = PermissaoUserController::Permissao($id_user, $idTela);
+        if ($validacao == "ok") {
+            $unidades = Unidades::all();
             $grupo_indicadores = GrupoIndicadores::all();
-            return view('indicadores/indicadores_novo', compact('unidades','grupo_indicadores'));
-		} else {
-			$validator = "Você não tem Permissão para acessar esta tela!!!";
-			return view('home')
-				->withErrors($validator);
-		}
+            return view('indicadores/indicadores_novo', compact('unidades', 'grupo_indicadores'));
+        } else {
+            $validator = "Você não tem Permissão para acessar esta tela!!!";
+            return view('home')
+                ->withErrors($validator);
+        }
     }
 
     public function storeIndicadores(Request $request)
@@ -134,90 +142,90 @@ class IndicadoresController extends Controller
         $input     = $request->all();
         $unidades  = Unidades::all();
         $grupo_indicadores = GrupoIndicadores::all();
-		$validator = Validator::make($request->all(), [
-			'grupo_id' => 'required|max:255',
+        $validator = Validator::make($request->all(), [
+            'grupo_id' => 'required|max:255',
             'status'   => 'required|max:255',
             'nome'     => 'required|max:255',
             'link'     => 'required|max:255'
-    	]);
-		if ($validator->fails()) {
-			return view('indicadores/indicadores_novo', compact('indicadores', 'grupo_indicadores','unidades'))
-					->withErrors($validator)
-					->withInput(session()->flashInput($request->input()));
-		}else {
-			$indicadores = Indicadores::create($input);
-			$indicadores = Indicadores::paginate(20);
+        ]);
+        if ($validator->fails()) {
+            return view('indicadores/indicadores_novo', compact('indicadores', 'grupo_indicadores', 'unidades'))
+                ->withErrors($validator)
+                ->withInput(session()->flashInput($request->input()));
+        } else {
+            $indicadores = Indicadores::create($input);
+            $indicadores = Indicadores::paginate(20);
             $grupo_indicadores = GrupoIndicadores::all();
             $id = Indicadores::all()->max('id');
-			$input['idTabela'] = $id;
-			$loggers   = Logger::create($input);
-			$validator = 'Indicador Cadastrado com Sucesso!';
-			return redirect()->route('cadastroIndicadores')
+            $input['idTabela'] = $id;
+            $loggers   = Logger::create($input);
+            $validator = 'Indicador Cadastrado com Sucesso!';
+            return redirect()->route('cadastroIndicadores')
                 ->withErrors($validator)
-                ->with('indicadores', 'grupo_indicadores','unidades');
-		}
-	}
+                ->with('indicadores', 'grupo_indicadores', 'unidades');
+        }
+    }
 
     public function indicadoresAlterar($id)
     {
         $id_user = Auth::user()->id;
-		$idTela = 4;
-		$validacao = PermissaoUserController::Permissao($id_user, $idTela);
-		if($validacao == "ok") {
-			$indicadores = Indicadores::where('id',$id)->get();
+        $idTela = 4;
+        $validacao = PermissaoUserController::Permissao($id_user, $idTela);
+        if ($validacao == "ok") {
+            $indicadores = Indicadores::where('id', $id)->get();
             $unidades    = Unidades::all();
-            $grupo_indicadores = GrupoIndicadores::orderby('nome','ASC')->get();
-            return view('indicadores/indicadores_alterar', compact('indicadores','unidades','grupo_indicadores'));
-		} else {
-			$validator = "Você não tem Permissão para acessar esta tela!!!";
-			return view('home')
-				->withErrors($validator);
-		}
+            $grupo_indicadores = GrupoIndicadores::orderby('nome', 'ASC')->get();
+            return view('indicadores/indicadores_alterar', compact('indicadores', 'unidades', 'grupo_indicadores'));
+        } else {
+            $validator = "Você não tem Permissão para acessar esta tela!!!";
+            return view('home')
+                ->withErrors($validator);
+        }
     }
 
     public function updateIndicadores($id, Request $request)
     {
         $input       = $request->all();
-	    $indicadores = Indicadores::where('id',$id)->get();
+        $indicadores = Indicadores::where('id', $id)->get();
         $unidades    = Unidades::all();
         $grupo_indicadores = GrupoIndicadores::all();
-		$validator = Validator::make($request->all(), [
-			'grupo_id' => 'required|max:255',
+        $validator = Validator::make($request->all(), [
+            'grupo_id' => 'required|max:255',
             'status'   => 'required|max:255',
             'nome'     => 'required|max:255',
             'link'     => 'required|max:255'
         ]);
-		if ($validator->fails()) {
-			return view('indicadores/indicadores_alterar', compact('indicadores','unidades','grupo_indicadores'))
-				->withErrors($validator)
-	    		->withInput(session()->flashInput($request->input()));
-		}else {
-			$indicadores = Indicadores::find($id); 
-			$indicadores->update($input);
-	    	$indicadores = Indicadores::paginate(20);
-            $input['idTabela'] = $id;
-			$loggers   = Logger::create($input);
-			$validator ='Indicador Alterado com Sucesso!';
-			return redirect()->route('cadastroIndicadores')
+        if ($validator->fails()) {
+            return view('indicadores/indicadores_alterar', compact('indicadores', 'unidades', 'grupo_indicadores'))
                 ->withErrors($validator)
-                ->with('indicadores', 'grupo_indicadores','unidades');
-		}
+                ->withInput(session()->flashInput($request->input()));
+        } else {
+            $indicadores = Indicadores::find($id);
+            $indicadores->update($input);
+            $indicadores = Indicadores::paginate(20);
+            $input['idTabela'] = $id;
+            $loggers   = Logger::create($input);
+            $validator = 'Indicador Alterado com Sucesso!';
+            return redirect()->route('cadastroIndicadores')
+                ->withErrors($validator)
+                ->with('indicadores', 'grupo_indicadores', 'unidades');
+        }
     }
 
     public function indicadoresExcluir($id)
     {
         $id_user = Auth::user()->id;
-		$idTela = 4;
-		$validacao = PermissaoUserController::Permissao($id_user, $idTela);
-		if($validacao == "ok") {
-			$indicadores = Indicadores::where('id',$id)->get();
+        $idTela = 4;
+        $validacao = PermissaoUserController::Permissao($id_user, $idTela);
+        if ($validacao == "ok") {
+            $indicadores = Indicadores::where('id', $id)->get();
             $grupo_indicadores = GrupoIndicadores::all();
-            return view('indicadores/indicadores_excluir', compact('indicadores','grupo_indicadores'));
-		} else {
-			$validator = "Você não tem Permissão para acessar esta tela!!!";
-			return view('home')
-				->withErrors($validator);
-		}
+            return view('indicadores/indicadores_excluir', compact('indicadores', 'grupo_indicadores'));
+        } else {
+            $validator = "Você não tem Permissão para acessar esta tela!!!";
+            return view('home')
+                ->withErrors($validator);
+        }
     }
 
     public function destroyIndicadores($id, Request $request)
@@ -225,13 +233,13 @@ class IndicadoresController extends Controller
         $input = $request->all();
         $unidades = Unidades::all();
         $input['idTabela'] = $id;
-		$loggers = Logger::create($input);
+        $loggers = Logger::create($input);
         Indicadores::find($id)->delete();
-		$indicadores       = Indicadores::paginate(20);
+        $indicadores       = Indicadores::paginate(20);
         $grupo_indicadores = GrupoIndicadores::all();
         $validator         = 'Indicador excluído com sucesso!';
-		return redirect()->route('cadastroIndicadores')
+        return redirect()->route('cadastroIndicadores')
             ->withErrors($validator)
-            ->with('indicadores', 'grupo_indicadores','unidades');
+            ->with('indicadores', 'grupo_indicadores', 'unidades');
     }
 }
